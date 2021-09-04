0679a6faee6f08738cc10eb4d6544baea6f129e4
ngnam <nguyenvannam0411@gmail.com>
2021-09-04 22:53:29 +0700

    Setup DB Context & Connection String

--- src/CommanderGQL/Startup.cs
+++ src/CommanderGQL/Startup.cs
@@ -1,6 +1,8 @@
+using CommanderGQL.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,9 @@ namespace CommanderGQL
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(opt => 
+                opt.UseSqlServer(Configuration.GetConnectionString("CommanderGQLDb"))
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

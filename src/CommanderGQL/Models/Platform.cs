72ce8c9672d61348bfbf303b774736f32af60f95
ngnam <nguyenvannam0411@gmail.com>
2021-09-06 00:36:19 +0700

    Adding in-line documentation to our API

--- src/CommanderGQL/Models/Platform.cs
+++ src/CommanderGQL/Models/Platform.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using HotChocolate;
 
 namespace CommanderGQL.Models
 {
+    [GraphQLDescription("Representer a Platform model")]
     public class Platform
     {
         public Platform()
@@ -14,6 +16,7 @@ namespace CommanderGQL.Models
 
         [Required]
         public string Name { get; set; }
+        [GraphQLDescription("Platform valid licensekey for the platform")]
         public string LisenceKey { get; set; }
         public ICollection<Command> Commands { get; set; }
     }

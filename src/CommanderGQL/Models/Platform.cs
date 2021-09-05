e6ad517b077e0bb9b231403e2c666216ed691ffe
ngnam <nguyenvannam0411@gmail.com>
2021-09-05 23:44:04 +0700

    Adding our 2nd Model

--- src/CommanderGQL/Models/Platform.cs
+++ src/CommanderGQL/Models/Platform.cs
@@ -1,14 +1,20 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace CommanderGQL.Models
 {
     public class Platform
     {
+        public Platform()
+        {
+            this.Commands = new HashSet<Command>();
+        }
         [Key]
         public int Id { get; set; }
-        
+
         [Required]
         public string Name { get; set; }
         public string LisenceKey { get; set; }
+        public ICollection<Command> Commands { get; set; }
     }
 }

04f963edeea1c5de08e492c2b305f5076b24ce53
ngnam <nguyenvannam0411@gmail.com>
2021-09-09 23:55:44 +0700

    Introducing Mutations & Adding Platforms

--- src/CommanderGQL/Startup.cs
+++ src/CommanderGQL/Startup.cs
@@ -33,6 +33,7 @@ namespace CommanderGQL
             services
                 .AddGraphQLServer()
                 .AddQueryType<Query>()
+                .AddMutationType<Mutation>()
                 .AddType<PlatformType>()
                 .AddType<CommandType>()
                 .AddFiltering()
